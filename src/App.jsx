4ff4627e8ef8ddc5127af97d1b3f98a6e79386b0
Joao Ribeiro <teste@teste.com>
2023-06-10 04:06:19 -0700

    hook useState 01

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import HookState01 from './components/UseState_01'
+
+export default () => {
+    return (
+        <>
+            <h1>REACT HOOKS</h1>
+            <HookState01 />
+        </>
+    )
+}
