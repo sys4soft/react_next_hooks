a7a8724c3b3194e7b964698b3881459df27c40f0
Joao Ribeiro <teste@teste.com>
2023-06-10 05:07:52 -0700

    03_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 // import HookState01 from './components/UseState_01'
-import HookState02 from './components/UseState_02'
+// import HookState02 from './components/UseState_02'
+import HookState03 from './components/UseState_03'
 
 export default () => {
     return (
         <>
             <h1>REACT HOOKS</h1>
             {/* <HookState01 /> */}
-            <HookState02 />
+            {/* <HookState02 /> */}
+            <HookState03 />
         </>
     )
 }
---
+++ src/components/UseState_03.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+export default () => {
+
+    const [valor1, setValor1] = useState(1);
+    const [valor2, setValor2] = useState(2);
+
+    function diminuir1() {
+        setValor1(v => v - 1);
+    }
+    function aumentar1() {
+        setValor1(v => v + 1);
+    }
+
+    function diminuir2() {
+        setValor2(v => v - 1);
+    }
+    function aumentar2() {
+        setValor2(v => v + 1);
+    }
+
+    return (
+        <>
+            <h4>useState - Exercício 1</h4>
+
+            <p>{valor1}</p>
+            <button onClick={diminuir1}>-</button>
+            <button onClick={aumentar1}>+</button>
+            <hr />
+            <p>{valor2}</p>
+            <button onClick={diminuir2}>-</button>
+            <button onClick={aumentar2}>+</button>
+            <hr />
+            <p>O resultado de {valor1} x {valor2} = {valor1 * valor2}</p>
+        </>
+    )
+}
