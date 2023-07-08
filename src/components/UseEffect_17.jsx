691a39dbd3614513fd070ff7ca76666ae20f4317
Joao Ribeiro <teste@teste.com>
2023-07-08 04:02:18 -0700

    17_hooks

--- src/components/UseEffect_17.jsx
+++ src/components/UseEffect_17.jsx
@@ -3,14 +3,31 @@ import React, { useState, useEffect } from 'react'
 export default () => {
 
     const [valor, setValor] = useState(0)
+    const [nome, setNome] = useState('')
 
     function incremento() {
         setValor(valor + 1)
     }
 
+    function definirNome(){
+        setNome('João Ribeiro')
+    }
+
     useEffect(() => {
-        console.log('componente iniciado 01')
+        console.log('Executar apenas no arranque do compoente')
     },[])
+
+    useEffect(() => {
+        console.log('Executar apenas na alteração do VALOR')
+    }, [valor])
+
+    useEffect(() => {
+        console.log('Executar apenas na alteração do NOME')
+    }, [nome])
+
+    useEffect(() => {
+        console.log('Executar sempre que VALOR e NOME sejam alterados')
+    }, [valor, nome])
 
 
     return (
@@ -18,6 +35,8 @@ export default () => {
             <h3>UseEffect 01</h3>
             <button onClick={incremento}>Incremento</button>
             <h3>{valor}</h3>
+            <button onClick={definirNome}>Definir nome</button>
+            <h3>{nome}</h3>
         </>
     )
 }
